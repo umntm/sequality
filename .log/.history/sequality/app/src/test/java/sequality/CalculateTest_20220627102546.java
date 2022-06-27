f0cbe57bd789461d51a92f44745706fd5e07f8b4
Umano Tomomichi <m1m22a08@st.oit.ac.jp>
2022-06-27 20:24:33 +0900

    add gradle project

---
+++ .log/.history/sequality/app/src/test/java/sequality/CalculateTest_20220627102546.java
@@ -0,0 +1,8 @@
+package sequality;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+
+}
---
+++ .log/.history/sequality/app/src/test/java/sequality/CalculateTest_20220627102613.java
@@ -0,0 +1,9 @@
+package sequality;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculateTest {
+  @Test
+
+}
